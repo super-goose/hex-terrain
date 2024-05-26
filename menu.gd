10946299272d430abce0a4ed12df92906771dc0b
Tex <james.davis@zenlist.com>
2024-05-26 09:14:00 -0500

    basic level editing

--- menu.gd
+++ menu.gd
@@ -27,3 +27,105 @@ func _on_grass_button_pressed():
 
 func _on_close_button_pressed():
 	hide_tile_menu()
+
+
+func _on_coast_a_button_pressed():
+	if current_node:
+		current_node.set_type('coast_a')
+
+
+
+func _on_coast_b_button_pressed():
+	if current_node:
+		current_node.set_type('coast_b')
+
+
+
+func _on_coast_c_button_pressed():
+	if current_node:
+		current_node.set_type('coast_c')
+
+
+
+func _on_coast_d_button_pressed():
+	if current_node:
+		current_node.set_type('coast_d')
+
+
+
+func _on_coast_e_button_pressed():
+	if current_node:
+		current_node.set_type('coast_e')
+
+
+
+func _on_river_a_button_pressed():
+	if current_node:
+		current_node.set_type('river_a')
+
+func _on_river_a_curvy_button_pressed():
+	if current_node:
+		current_node.set_type('river_a_curvy')
+
+func _on_river_b_button_pressed():
+	if current_node:
+		current_node.set_type('river_b')
+
+func _on_river_c_button_pressed():
+	if current_node:
+		current_node.set_type('river_c')
+
+func _on_river_d_button_pressed():
+	if current_node:
+		current_node.set_type('river_d')
+
+func _on_river_e_button_pressed():
+	if current_node:
+		current_node.set_type('river_e')
+
+func _on_river_f_button_pressed():
+	if current_node:
+		current_node.set_type('river_f')
+
+func _on_river_g_button_pressed():
+	if current_node:
+		current_node.set_type('river_g')
+
+func _on_river_h_button_pressed():
+	if current_node:
+		current_node.set_type('river_h')
+
+func _on_river_i_button_pressed():
+	if current_node:
+		current_node.set_type('river_i')
+
+func _on_river_j_button_pressed():
+	if current_node:
+		current_node.set_type('river_j')
+
+func _on_river_k_button_pressed():
+	if current_node:
+		current_node.set_type('river_k')
+
+func _on_river_l_button_pressed():
+	if current_node:
+		current_node.set_type('river_l')
+
+func _on_river_crossing_a_button_pressed():
+	if current_node:
+		current_node.set_type('river_crossing_a')
+
+func _on_river_crossing_b_button_pressed():
+	if current_node:
+		current_node.set_type('river_crossing_b')
+
+
+func _on_ccw_button_pressed():
+	if current_node:
+		current_node.rotate_tile('ccw')
+
+
+func _on_cw_button_pressed():
+	if current_node:
+		current_node.rotate_tile('cw')
+
--- tile.gd
+++ tile.gd
@@ -13,6 +13,21 @@ var HexCoastC = load("res://assets/land/coast/hex_coast_C.gltf") # water at posi
 var HexCoastD = load("res://assets/land/coast/hex_coast_D.gltf") # water at position 1, 2, 3, 4
 var HexCoastE = load("res://assets/land/coast/hex_coast_E.gltf") # no water, but coast at 3.5
 
+var HexRiverA = load("res://assets/land/rivers/hex_river_A.gltf")
+var HexRiverACurvy = load("res://assets/land/rivers/hex_river_A_curvy.gltf")
+var HexRiverB = load("res://assets/land/rivers/hex_river_B.gltf")
+var HexRiverC = load("res://assets/land/rivers/hex_river_C.gltf")
+var HexRiverD = load("res://assets/land/rivers/hex_river_D.gltf")
+var HexRiverE = load("res://assets/land/rivers/hex_river_E.gltf")
+var HexRiverF = load("res://assets/land/rivers/hex_river_F.gltf")
+var HexRiverG = load("res://assets/land/rivers/hex_river_G.gltf")
+var HexRiverH = load("res://assets/land/rivers/hex_river_H.gltf")
+var HexRiverI = load("res://assets/land/rivers/hex_river_I.gltf")
+var HexRiverJ = load("res://assets/land/rivers/hex_river_J.gltf")
+var HexRiverK = load("res://assets/land/rivers/hex_river_K.gltf")
+var HexRiverL = load("res://assets/land/rivers/hex_river_L.gltf")
+var HexRiverCrossingA = load("res://assets/land/rivers/hex_river_crossing_A.gltf")
+var HexRiverCrossingB = load("res://assets/land/rivers/hex_river_crossing_B.gltf")
 
 func _ready():
 	set_mesh()
@@ -37,6 +52,12 @@ func set_coords(x, z):
 func set_get_neighbors(callback: Callable):
 	get_neighbors = callback
 
+func rotate_tile(d):
+	if d == 'ccw':
+		rotate_y(PI / 3)
+	elif d == 'cw':
+		rotate_y(-PI / 3)
+
 func set_mesh():
 	for child in $MeshContainer.get_children():
 		$MeshContainer.remove_child(child)
@@ -45,3 +66,41 @@ func set_mesh():
 			$MeshContainer.add_child(HexGrass.instantiate())
 		'water':
 			$MeshContainer.add_child(HexWater.instantiate())
+		'coast_a':
+			$MeshContainer.add_child(HexCoastA.instantiate())
+		'coast_b':
+			$MeshContainer.add_child(HexCoastB.instantiate())
+		'coast_c':
+			$MeshContainer.add_child(HexCoastC.instantiate())
+		'coast_d':
+			$MeshContainer.add_child(HexCoastD.instantiate())
+		'coast_e':
+			$MeshContainer.add_child(HexCoastD.instantiate())
+		'river_a':
+			$MeshContainer.add_child(HexRiverA.instantiate())
+		'river_a_curvy':
+			$MeshContainer.add_child(HexRiverACurvy.instantiate())
+		'river_b':
+			$MeshContainer.add_child(HexRiverB.instantiate())
+		'river_c':
+			$MeshContainer.add_child(HexRiverC.instantiate())
+		'river_d':
+			$MeshContainer.add_child(HexRiverD.instantiate())
+		'river_e':
+			$MeshContainer.add_child(HexRiverE.instantiate())
+		'river_f':
+			$MeshContainer.add_child(HexRiverF.instantiate())
+		'river_g':
+			$MeshContainer.add_child(HexRiverG.instantiate())
+		'river_h':
+			$MeshContainer.add_child(HexRiverH.instantiate())
+		'river_i':
+			$MeshContainer.add_child(HexRiverI.instantiate())
+		'river_j':
+			$MeshContainer.add_child(HexRiverJ.instantiate())
+		'river_k':
+			$MeshContainer.add_child(HexRiverK.instantiate())
+		'river_l':
+			$MeshContainer.add_child(HexRiverL.instantiate())
+		_:
+			print('not implemented:', type)
